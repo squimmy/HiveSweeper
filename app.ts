b29e9ad3764d1888c1a4e80bc7781e41d591e099
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-07-29 04:01:20 +0800

    use classes

--- app.ts
+++ app.ts
@@ -1,12 +1,46 @@
 ﻿angular.module('hivesweeper', [])
     .controller('HiveController', ($scope) => {
         $scope.tiles = [
-            { x: 0, y: 0 },
-            { x: 1, y: 0 },
-            { x: -1, y: 0 },
-            { x: 0, y: 1 },
-            { x: 0, y: -1 },
-            { x: 1, y: 1 },
-            { x: -1, y: -1 }
+            new Tile(0, 0, false),
+            new Tile(1, 0, false),
+            new Tile(-1, 0, false),
+            new Tile(0, 1, false),
+            new Tile(0, -1, false),
+            new Tile(1, 1, false),
+            new Tile(-1, -1, false)
         ];
     });
+
+class Point {
+    public x: number;
+    public y: number;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Tile {
+    public coords: Point;
+    public isMine: boolean;
+    public neighbours: Point[];
+
+    constructor(x, y, isMine) {
+        this.coords = new Point(x, y);
+        this.isMine = isMine;
+        this.neighbours = this.getNeighbours();
+    }
+
+    private getNeighbours() {
+        var relativeNeighbours = [
+            new Point(1, 0),
+            new Point(1, 1),
+            new Point(0, -1),
+            new Point(0, 1),
+            new Point(-1, 0),
+            new Point(-1, -1)
+        ];
+        return _.map(relativeNeighbours, p => new Point(p.x + this.coords.x, p.y + this.coords.y));
+    }
+}
