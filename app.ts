8cbab4ae068da76256bc289d8fa561fc6510bb02
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-07-29 03:40:17 +0800

    initial commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+﻿angular.module('hivesweeper', [])
+    .controller('HiveController', ($scope) => {
+        $scope.tiles = [
+            { x: 0, y: 0 },
+            { x: 1, y: 0 },
+            { x: -1, y: 0 },
+            { x: 0, y: 1 },
+            { x: 0, y: -1 },
+            { x: 1, y: 1 },
+            { x: -1, y: -1 }
+        ];
+    });
