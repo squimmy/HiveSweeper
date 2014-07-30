12def45143f3d1cea4093ab4a1d43b858c3f8192
Timothy du Heaume <timothy.duheaume@gmail.com>
2014-07-31 03:21:02 +0800

    dynamically resize grid to fit available space

--- app.ts
+++ app.ts
@@ -1,15 +1,63 @@
 ﻿angular.module('hivesweeper', ['directives'])
-    .controller('HiveController', ($scope) => {
+    .controller('HiveController', ($scope, $timeout) => {
         var radius = 2;
         $scope.tiles = _.flatten(_.map(
             _.range(-radius, radius + 1),
             x => _.map(
                 _.range(Math.max(-radius, x - radius), Math.min(radius, x + radius) + 1),
                 y => new Tile(y, x, false))));
-
         _.forEach(_.sample($scope.tiles, 6), t => t['isMine'] = true);
         _.forEach($scope.tiles, t => t['init']($scope.tiles));
+
+        $scope.transform = new Transform();
+
+        var svg: any = document.getElementsByTagName('svg')[0];
+        $scope.updateTransform = () => {
+            var root = svg.firstElementChild;
+            if (root != null) {
+                var parentRect = svg.getBoundingClientRect();
+                var childRect = root.getBoundingClientRect();
+                $scope.$apply(() => {
+                    $scope.transform.update(
+                        parentRect.width,
+                        parentRect.height,
+                        childRect.width,
+                        childRect.height);
+                });
+            }
+        };
+        angular.element(window).bind('resize', $scope.updateTransform);
+        $timeout($scope.updateTransform, 50);
     });
+
+class Transform {
+    private offsetX: number;
+    private offsetY: number;
+    private scale: number;
+
+    constructor() {
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.scale = 1;
+    }
+
+    public update(parentX: number, parentY: number, childX: number, childY: number) {
+        childX /= this.scale;
+        childY /= this.scale;
+
+        var rX = parentX / childX;
+        var rY = parentY / childY;
+
+        this.scale = Math.min(rX, rY) * 0.9;
+
+        this.offsetX = (childX / 2) * this.scale * 1.05;
+        this.offsetY = (childY / 2) * this.scale * 1.05;
+    }
+
+    public text() {
+        return 'translate(' + this.offsetX + ', ' + this.offsetY + ') scale(' + this.scale + ')';
+    }
+}
 
 class Point {
     public x: number;
